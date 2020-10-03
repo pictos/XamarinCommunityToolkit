efb67e9d9f67f2018f9f9ca53f176ffe92f0985e
Pedro Jesus <pedrojesus.cefet@gmail.com>
2020-10-03 11:55:00 -0300

    Added Action call in my benchmark test

---
+++ XCTBenchmark/XCTBenchmark/Benchmarks/DelegatesBench.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace XCTBenchmark.Benchmarks
+{
+	[MemoryDiagnoser]
+	public class DelegatesBench
+	{
+		Action action = () =>
+		{
+			if (DateTime.Now.Ticks < 0)
+				Console.WriteLine("Helloe");
+		};
+
+		public static void RunAction(Action action)
+		{
+			action();
+		}
+
+		[Benchmark]
+		public void NormalCall()
+		{
+			RunAction(action);
+		}
+
+		[Benchmark]
+		public void LambdalCall()
+		{
+			RunAction(() => action());
+		}
+	}
+}
